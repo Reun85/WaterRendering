3eb40ae4807948d2b40d9fdf071a77dc369b2ada
Reun85 <gugigergo@gmail.com>
2024-07-21 19:14:00 +0200

    Added a way to create an ImmutableTexture from TextureData

--- Axodox.Graphics.Shared/Graphics/D3D12/Resources/ImmutableTexture.cpp
+++ Axodox.Graphics.Shared/Graphics/D3D12/Resources/ImmutableTexture.cpp
@@ -3,21 +3,30 @@
 
 using namespace std;
 
-namespace Axodox::Graphics::D3D12
-{
-  ImmutableTexture::ImmutableTexture(const ResourceAllocationContext& context, const std::filesystem::path& path)
-  {
-    auto textureData = TextureData::FromFile(path);
-    _texture = context.ResourceAllocator->CreateTexture(textureData.Definition());
+namespace Axodox::Graphics::D3D12 {
+ImmutableTexture::ImmutableTexture(const ResourceAllocationContext &context,
+                                   const std::filesystem::path &path) {
+  auto textureData = TextureData::FromFile(path);
+  _texture = context.ResourceAllocator->CreateTexture(textureData.Definition());
 
-    _allocatedSubscription = _texture->Allocated([this, context, data = move(textureData)](Resource* resource) {
-      context.ResourceUploader->EnqueueUploadTask(resource, &data);
-      _view = context.CommonDescriptorHeap->CreateShaderResourceView(resource);
+  _allocatedSubscription = _texture->Allocated(
+      [this, context, data = move(textureData)](Resource *resource) {
+        context.ResourceUploader->EnqueueUploadTask(resource, &data);
+        _view =
+            context.CommonDescriptorHeap->CreateShaderResourceView(resource);
       });
-  }
-
-  ImmutableTexture::operator GpuVirtualAddress() const
-  {
-    return *_view;
-  }
 }
+ImmutableTexture::ImmutableTexture(const ResourceAllocationContext &context,
+                                   const TextureData &textureData) {
+  _texture = context.ResourceAllocator->CreateTexture(textureData.Definition());
+
+  _allocatedSubscription = _texture->Allocated(
+      [this, context, data = move(textureData)](Resource *resource) {
+        context.ResourceUploader->EnqueueUploadTask(resource, &data);
+        _view =
+            context.CommonDescriptorHeap->CreateShaderResourceView(resource);
+      });
+}
+
+ImmutableTexture::operator GpuVirtualAddress() const { return *_view; }
+} // namespace Axodox::Graphics::D3D12
--- Axodox.Graphics.Shared/Graphics/D3D12/Resources/ImmutableTexture.h
+++ Axodox.Graphics.Shared/Graphics/D3D12/Resources/ImmutableTexture.h
@@ -2,18 +2,19 @@
 #include "Texture.h"
 #include "ResourceAllocationContext.h"
 
-namespace Axodox::Graphics::D3D12
-{
-  class AXODOX_GRAPHICS_API ImmutableTexture
-  {
-  public:
-    ImmutableTexture(const ResourceAllocationContext& context, const std::filesystem::path& path);
+namespace Axodox::Graphics::D3D12 {
+class AXODOX_GRAPHICS_API ImmutableTexture {
+public:
+  ImmutableTexture(const ResourceAllocationContext &context,
+                   const std::filesystem::path &path);
+  ImmutableTexture(const ResourceAllocationContext &context,
+                   const TextureData &textureData);
 
-    operator GpuVirtualAddress() const;
+  operator GpuVirtualAddress() const;
 
-  private:
-    TextureRef _texture;
-    ShaderResourceViewRef _view;
-    Infrastructure::event_subscription _allocatedSubscription;
-  };
-}
+private:
+  TextureRef _texture;
+  ShaderResourceViewRef _view;
+  Infrastructure::event_subscription _allocatedSubscription;
+};
+} // namespace Axodox::Graphics::D3D12
