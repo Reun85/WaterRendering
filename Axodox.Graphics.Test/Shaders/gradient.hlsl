904bbe5c6d2275edebe21129ef9f87e07180cf48
Reun85 <gugigergo@gmail.com>
2024-09-08 13:14:19 +0200

    Gradients use shared memory for better performance

--- Axodox.Graphics.Test/Shaders/gradient.hlsl
+++ Axodox.Graphics.Test/Shaders/gradient.hlsl
@@ -5,25 +5,50 @@ cbuffer Test : register(b9)
 {
     ComputeConstants constants;
 }
-[numthreads(16, 16, 1)]
-void main(uint3 dispatchThreadID : SV_DispatchThreadID)
+
+
+#define N DISP_MAP_SIZE
+#define LOG2_M 4
+#define M (1<<LOG2_M)
+#define MHalf (M>>1)
+#define LOG2_N_DIV_M (DISP_MAP_LOG2-LOG2_M)
+#define N_DIV_M (1 << LOG2_N_DIV_M)
+
+
+
+groupshared float3 cache[M + 2][M + 2];
+
+
+// The +2 are overhangs
+[numthreads(M + 2, M + 2, 1)]
+void main(uint3 DTid : SV_DispatchThreadID, uint3 LTid : SV_GroupThreadID, uint3 GTid : SV_GroupID)
 {
 
     const float PATCH_SIZE = constants.patchSize;
     const float TILE_SIZE_X2 = PATCH_SIZE * 2.0f / float(DISP_MAP_SIZE);
     const float INV_TILE_SIZE = float(DISP_MAP_SIZE) / PATCH_SIZE;
+    
+    
+    
+    int2 groupID = GTid.xy;
+    int2 threadID = LTid.xy;
 
-    int2 loc = int2(dispatchThreadID.xy);
+    
+    int2 loc = (groupID * M + threadID.xy - 1) & (DISP_MAP_SIZE - 1);
+    cache[threadID.x][threadID.y] = displacement[loc].xyz;
 
-    int2 left = (loc - int2(1, 0)) & (DISP_MAP_SIZE - 1);
-    int2 right = (loc + int2(1, 0)) & (DISP_MAP_SIZE - 1);
-    int2 bottom = (loc - int2(0, 1)) & (DISP_MAP_SIZE - 1);
-    int2 top = (loc + int2(0, 1)) & (DISP_MAP_SIZE - 1);
+        // These threads only existed to read from texture
+    if (threadID.x == 0 || threadID.y == 0 || threadID.x == M + 1 || threadID.y == M + 1)
+    {
+        return;
+    }
+    GroupMemoryBarrierWithGroupSync();
 
-    float3 disp_left = displacement[left].xyz;
-    float3 disp_right = displacement[right].xyz;
-    float3 disp_bottom = displacement[bottom].xyz;
-    float3 disp_top = displacement[top].xyz;
+    
+    float3 disp_left = cache[threadID.x - 1][threadID.y];
+    float3 disp_right = cache[threadID.x + 1][threadID.y];
+    float3 disp_bottom = cache[threadID.x][threadID.y - 1];
+    float3 disp_top = cache[threadID.x][threadID.y + 1];
     
     float3 dv = disp_right - disp_left + float3(TILE_SIZE_X2, 0, 0);
     float3 du = disp_top - disp_bottom + float3(0, 0, TILE_SIZE_X2);
